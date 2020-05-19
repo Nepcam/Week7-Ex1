90dfc7da3266bc15d42def550a157a62b9c45151
Nepcam <cameron.nepe@gmail.com>
2020-05-19 14:56:40 +1200

    Add project files.

---
+++ PracTest2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace PracTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.displayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawBoardMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(12, 34);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(710, 625);
+            this.pictureBoxDisplay.TabIndex = 0;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.displayToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(734, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // displayToolStripMenuItem
+            // 
+            this.displayToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.drawBoardMenu,
+            this.toolStripTextBox1,
+            this.toolStripMenuItem1,
+            this.clearToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.displayToolStripMenuItem.Name = "displayToolStripMenuItem";
+            this.displayToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.displayToolStripMenuItem.Text = "Display";
+            // 
+            // drawBoardMenu
+            // 
+            this.drawBoardMenu.Name = "drawBoardMenu";
+            this.drawBoardMenu.Size = new System.Drawing.Size(180, 22);
+            this.drawBoardMenu.Text = "Draw Board";
+            this.drawBoardMenu.Click += new System.EventHandler(this.drawBoardMenu_Click);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 735);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "103 Practical Test 2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem displayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drawBoardMenu;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+    }
+}
+
---
+++ PracTest2/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PracTest2
+{
+    public partial class Form1 : Form
+    {
+        //Name: Cameron Nepe
+        //ID: 1262199
+
+        //The minimum size of the board
+        int MIN_BOARD_SIZE = 2;
+        //The maximum size of the board
+        int MAX_BOARD_SIZE = 10;
+
+        //Width of a square
+        int WIDTH = 50;
+        //Height of a square
+        int HEIGHT = 50;
+
+        //the colour of a Dark square (a variable since Color cannot be a const)
+        Color DarkBrown = Color.SaddleBrown;
+
+        //the colour of a Light square (a variable since Color cannot be a const)
+        Color LightBrown = Color.SandyBrown;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Draw a square at the given x and y position and size and in the given color 
+        /// </summary>
+        /// <param name="paper">Where to draw the graphics</param>
+        /// <param name="x">The x position of the top left corner</param>
+        /// <param name="y">The y position of the top left corner</param>
+        /// <param name="length">The width and height of the square</param>
+        /// <param name="squareColor">The fill color of the square</param>
+        private void DrawSquare(Graphics paper, int x, int y, int width, int height, Color squareColor)
+        {
+            //Graphics paper = pictureBoxDisplay.CreateGraphics();
+            SolidBrush br = new SolidBrush(squareColor);
+            Pen pen1 = new Pen(Color.Black, 2);
+
+            paper.FillRectangle(br, x, y, width, height);
+            paper.DrawRectangle(pen1, x, y, width, height);
+        }
+
+        private void drawBoardMenu_Click(object sender, EventArgs e)
+        {
+            Graphics canvas = pictureBoxDisplay.CreateGraphics();
+            int xpos = 0;
+            int ypos = 0;
+
+            DrawSquare(canvas, xpos, ypos, WIDTH, HEIGHT, DarkBrown);
+            //xpos += WIDTH;
+            //DrawSquare(canvas, xpos, ypos, WIDTH, HEIGHT, LightBrown);
+        }
+
+        /// <summary>
+        /// Draw a row at the given x and y position and in the given color
+        /// </summary>
+        /// <param name="paper">Where to draw the graphics</param>
+        /// <param name="boardSize">Size of board</param>
+        /// <param name="rowNum">Current row number</param>
+        /// <param name="y">The y position of the top left corner</param>
+        private void DrawRow(Graphics paper, int boardSize, int rowNum, int y)
+        {
+            //Declare variables
+            Graphics canvas1 = pictureBoxDisplay.CreateGraphics();
+            int xpos = 0;
+            int ypos = 0;
+
+            //FOR each column to draw
+            for (int i = 0; i <= MAX_BOARD_SIZE; i++)
+            {
+                //IF(row num + col num) % 2 == 0 THEN
+                if ()
+                {
+                    //Call DrawSquare with LightBrown colour
+                    DrawSquare(canvas1, xpos, ypos, WIDTH, HEIGHT, LightBrown);
+                }                  
+                //ELSE
+                else
+                {
+                    //Call DrawSquare with DarkBrown colour
+                    DrawSquare(canvas1, xpos, ypos, WIDTH, HEIGHT, DarkBrown);
+                }
+                //ENDIF
+                //Shift x to the right by width of square
+                xpos += WIDTH;
+            }
+            //ENDFOR
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripTextBox1.Clear();
+            toolStripTextBox1.Focus();
+            pictureBoxDisplay.Refresh();
+        }
+    }
+}
