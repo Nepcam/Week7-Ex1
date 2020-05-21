7c6717065a9e65e1bf57dded0aa681cbc910c731
Nepcam <cameron.nepe@gmail.com>
2020-05-21 19:30:18 +1200

    exercise complete

--- PracTest2/Form1.cs
+++ PracTest2/Form1.cs
@@ -46,7 +46,6 @@ namespace PracTest2
         /// <param name="squareColor">The fill color of the square</param>
         private void DrawSquare(Graphics paper, int x, int y, int width, int height, Color squareColor)
         {
-            //Graphics paper = pictureBoxDisplay.CreateGraphics();
             SolidBrush br = new SolidBrush(squareColor);
             Pen pen1 = new Pen(Color.Black, 2);
 
@@ -57,46 +56,44 @@ namespace PracTest2
         private void drawBoardMenu_Click(object sender, EventArgs e)
         {
             Graphics canvas = pictureBoxDisplay.CreateGraphics();
-            int xpos = 0;
+            //int xpos = 0;
             int ypos = 0;
-            int rowNum = 0; //int.Parse(toolStripTextBox1.Text);
+            //GET number of squares 
+            int colNum = int.Parse(toolStripTextBox1.Text);
 
-            //DrawSquare(canvas, xpos, ypos, WIDTH, HEIGHT, DarkBrown);
-            //xpos += WIDTH;
-            //DrawSquare(canvas, xpos, ypos, WIDTH, HEIGHT, LightBrown);
-            DrawRow(canvas, MAX_BOARD_SIZE, rowNum, ypos);
+            //DrawSquare(canvas, xpos, ypos, WIDTH, HEIGHT, LightBrown); Test DrawSquare() 
+            for (int rowNum = 0; rowNum < colNum; rowNum++)
+            {
+                DrawRow(canvas, colNum, rowNum, ypos);
+                ypos += HEIGHT;
+            }
         }
 
         /// <summary>
         /// Draw a row at the given x and y position and in the given color
         /// </summary>
-        /// <param name="paper">Where to draw the graphics</param>
+        /// <param name="canvas">Where to draw the graphics</param>
         /// <param name="boardSize">Size of board</param>
         /// <param name="currentRowNum">Current row number</param>
         /// <param name="y">The y position of the top left corner</param>
         private void DrawRow(Graphics canvas, int boardSize, int currentRowNum, int y)
         {
             //Declare variables
-            //Graphics canvas1 = pictureBoxDisplay.CreateGraphics();
             int xpos = 0;
-            int ypos = 0;
-            //GET number of squares 
-            int rowNum = int.Parse(toolStripTextBox1.Text);
-            int colNum = 0;
-
+           
             //FOR each column to draw
-            for (int i = 0; i <= MAX_BOARD_SIZE; i++)
+            for (int colNum = 0; colNum < boardSize; colNum++)
             {
                 //IF(row num + col num) % 2 == 0 THEN
-                if ((rowNum + colNum) % 2 == 0) // Working on this statement
+                if ((currentRowNum + colNum) % 2 == 0) // Working on this statement
                 {
                     //Call DrawSquare with LightBrown colour
-                    DrawSquare(canvas, xpos, ypos, WIDTH, HEIGHT, LightBrown);
+                    DrawSquare(canvas, xpos, y, WIDTH, HEIGHT, LightBrown);
                 }
                 else
                 {
                     //Call DrawSquare with DarkBrown colour
-                    DrawSquare(canvas, xpos, ypos, WIDTH, HEIGHT, DarkBrown);
+                    DrawSquare(canvas, xpos, y, WIDTH, HEIGHT, DarkBrown);
                 }
                 //ENDIF
                 //Shift x to the right by width of square
